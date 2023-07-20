9d25c670aefbbc765d4c04e53023cfbf4f77f38f
Resized <shai.yarden@gmail.com>
2023-07-20 05:33:37 +0300

    added whitenoise

--- project/settings.py
+++ project/settings.py
@@ -46,6 +46,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'project.urls'
@@ -115,7 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Use BigAutoField as the default auto field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
